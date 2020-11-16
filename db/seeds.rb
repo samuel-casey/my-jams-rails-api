d54d5eb9a8e7133eee2813faa19d0451541d66db
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-16 10:22:28 -0500

    seed DB locally and open cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+    Song.create({
+        title: Faker::Music::Prince.song,
+        artist: 'Prince',
+        length_mins_seconds: '3:35'
+    })
+end
+
+3.times do
+    Song.create({
+        title: Faker::Music::Phish.song,
+        artist: 'Phish',
+        length_mins_seconds: '5:45'
+    })
+end
+
+4.times do
+    Song.create({
+        title: Faker::Music::GratefulDead.song,
+        artist: 'Grateful Dead',
+        length_mins_seconds: '4:30'
+    })
+end
